c7fe4fce10087a058efa97d8ec0de62f9ab6059c
Uttam Marakana <uttammarakana03@gmail.com>
2026-08-11 00:01:25 +0530

    Modify Code : : "services/"

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -17,5 +17,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const analytics = getAnalytics(app)
 export default app;

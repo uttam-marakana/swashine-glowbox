5cd915239e847d303e41899f3c2327307452089d
Uttam Marakana <uttammarakana03@gmail.com>
2026-08-13 23:58:00 +0530

    Modified : : "main.jsx"

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
-import { ThemeProvider } from './context/ThemeContext.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
+import App from './App.jsx';
+import './index.css';
+import { ThemeProvider } from './context/ThemeContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
         <App />
+        <Analytics />
       </ThemeProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);

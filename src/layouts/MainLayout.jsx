0c71592584e4d583d7b268f7c39484ec68dafbe2
Uttam Marakana <uttammarakana03@gmail.com>
2026-08-14 00:19:20 +0530

    Modified : : Layout/"

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from '@/components/global/Header';
 import Footer from '@/components/global/Footer';
+import ScrollToTop from '@/components/global/ScrollToTop';
 
 export default function MainLayout() {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
       <Header />
       <main className="flex-1">
         <Outlet />

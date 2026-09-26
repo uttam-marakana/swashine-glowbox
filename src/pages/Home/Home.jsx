3e6d84fda817985e1440194bb4ceb23c12dc1e7f
uttam-marakana <uttammarakana03@gmail.com>
2026-09-26 15:14:30 +0530

    Modify : : "Home"  file

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -188,22 +188,6 @@ function HowItWorksSection() {
           ))}
         </div>
 
-        {/* <div className="flex justify-center gap-2 mb-8">
-          {howItWorks.map((_, i) => (
-            <button
-              key={i}
-              type="button"
-              aria-label={`Step ${i + 1}`}
-              onClick={() => setActive(i)}
-              className={`h-1.5 rounded-full transition-all ${
-                active === i
-                  ? "w-8 bg-brand-400"
-                  : "w-3 bg-white/15 hover:bg-white/25"
-              }`}
-            />
-          ))}
-        </div> */}
-
         <div className="text-center mt-10">
           <Button href="/custom">Start with size calculator</Button>
         </div>
